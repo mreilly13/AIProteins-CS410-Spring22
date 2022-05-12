6c8f0b053ac4e87cba110be0d1687491bb0d5037
Francisco <70050147+1aidea@users.noreply.github.com>
2022-05-12 18:11:14 -0400

    changed plotting to plot entire dataset

--- NNModel/launch_model.py
+++ NNModel/launch_model.py
@@ -1,14 +1,15 @@
-from NNModel.black_box import load_single_data, load_model, run_model
+from NNModel.black_box import load_data, load_single_data, load_model, run_model
 import NNModel.util.graphs as graphs
 
 
 def load(data):
     # load data from single file
     dataset = load_single_data(data) # put file name here
+    large_dataset = load_data()
     print("LOADING IN MODEL")
     model = load_model("YBYF_Model_1")
     regression_preped_dataset = run_model(model, dataset)
-    graphs.plotData(regression_preped_dataset[0])
+    graphs.plotData(dataset[0])
 
 if __name__=="__main__":
     load()
