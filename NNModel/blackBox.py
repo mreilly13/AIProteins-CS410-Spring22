2bacb45bb09cc1866776f43b22b8c80ddce66a11
Francisco <70050147+1aidea@users.noreply.github.com>
2022-05-17 15:56:51 -0400

    bug fix

--- NNModel/blackBox.py
+++ NNModel/blackBox.py
@@ -93,11 +93,12 @@ def load_ss_data():
 
     noise_features = non_ss_features[:noise, :]
     noise_labels = non_ss_labels[:noise]
-    features = np.append(ss_features, [noise_features], axis= 0)
-    labels = np.append(ss_labels, [noise_labels], axis= 0)
     print(ss_features.shape, ss_labels.shape)
     print(noise_features.shape, noise_labels.shape)
     print(features.shape, labels.shape)
+    
+    features = np.append(ss_features, [noise_features], axis= 0)
+    labels = np.append(ss_labels, [noise_labels], axis= 0)
     
     #print(ss_labels.shape, ss_features.shape)
     return [features, labels]
