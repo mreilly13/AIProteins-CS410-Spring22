5050411602e9f96266aba5ab252fddb549791d5a
mreilly13 <mrreilly2013@gmail.com>
2022-05-18 18:40:27 -0400

    iterating model

--- NNModel/blackBox.py
+++ NNModel/blackBox.py
@@ -103,7 +103,7 @@ def load_ss_data():
     non_ss_features = np.array(non_ss_features)
     non_ss_labels = np.zeros((len(non_ss_features)))
 
-    noise_ratio = .005
+    noise_ratio = .001
     noise = int(len(non_ss_features) * noise_ratio)
 
     noise_features = non_ss_features[:noise, :]
