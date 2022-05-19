55a3787c7f3e2468370a1e1a66d45af7be839e1f
mreilly13 <mrreilly2013@gmail.com>
2022-05-19 11:46:50 -0400

    improving inline comments

--- NNModel/launchModel.py
+++ NNModel/launchModel.py
@@ -2,6 +2,7 @@ from NNModel.blackBox import load_single_data, run_NNModel
 from NNModel.Util.graphs import plot_data
 
 def test(data, name, NNModel):
+    # evaluate a single PDB with the passed model
     dataset = load_single_data(data)
     plot_data(dataset[0], name)
     return run_NNModel(NNModel, dataset)
